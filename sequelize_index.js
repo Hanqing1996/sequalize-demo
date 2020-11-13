e0e080e20e8958f12bcc995868d66e0f9d2ccf4f
Hanqing1996 <zhq199622@163.com>
2020-11-13 18:25:45 +0800

    sequelize:add/delete/find record in users table

--- sequelize_index.js
+++ sequelize_index.js
@@ -23,7 +23,38 @@ User.init({
     modelName: 'User' // We need to choose the model name
 });
 
-const createUserTable=async function(){
+const createTable=async function(){
     await User.sync({ force: true });
 }
-createUserTable()
+// createTable()
+
+const dropTable=async function(){
+    await User.drop()
+}
+// dropTable()
+
+
+const insertRecord=async function () {
+    const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
+    console.log("Jane", jane);
+}
+
+// insertRecord()
+
+
+const findRecords=async function(){
+    const records=await User.findAll()
+    console.log("All users:", JSON.stringify(records, null, 2));
+    
+}
+// findRecords()
+
+const deleteRecord=async function(){
+    const result=await User.destroy({
+        where: {
+            firstName: "Jane"
+        }
+    });
+    console.log(result)
+}
+// deleteRecord()
